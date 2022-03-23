1ef3b22d302dbaf1c5cde70f830cabc3602a495c
Platy <daryl.limkz@gmail.com>
2022-03-23 20:59:51 +0800

    refactor(init): added defaults namespace
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- include/init.hpp
+++ include/init.hpp
@@ -6,11 +6,6 @@ using std::string;
 
 class Init {
   private:
-    const static string directory;
-    const static string objects;
-    const static string refs;
-    const static string heads;
-
     static bool exists();
     static bool createTree();
 
