2cbea00582a184c89dd0eb6848119c7ed8ae4d7d
Platy <daryl.limkz@gmail.com>
2022-04-01 09:33:21 +0800

    refactor(diff): made diff header only
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- include/diff.hpp
+++ include/diff.hpp
@@ -1,13 +1,23 @@
 #pragma once
 
+#include "diff_match_patch.h"
+
 #include <iostream>
 
 using namespace std;
 
 class Diff {
   public:
-    static string diff(string prev, string curr);
-    static string patch(string patch, string text);
-    static bool isDiff(string prev, string curr);
-    static string unpatch(string patch, string curr);
+    static string diff(string prev, string curr) {
+        diff_match_patch<string> dmp;
+        return dmp.patch_toText(dmp.patch_make(prev, curr));
+    }
+
+    static string patch(string patch, string prev) {
+        diff_match_patch<string> dmp;
+        return dmp.patch_apply(dmp.patch_fromText(patch), prev).first;
+    }
+    static bool isDiff(string prev, string curr) {
+        return Diff::diff(prev, curr) == "";
+    }
 };
