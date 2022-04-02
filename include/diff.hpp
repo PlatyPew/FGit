387366ccece1ce2a03d3509b5173b81335a25f49
Platy <daryl.limkz@gmail.com>
2022-04-02 12:39:39 +0800

    docs(diff): added docs for diff
    BREAKING CHANGE: isDiff returns true when there is a difference (used to return false)
    
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- include/diff.hpp
+++ include/diff.hpp
@@ -6,16 +6,40 @@
 
 class Diff {
   public:
+    /**
+     * @brief creates a unidiff patch between text1 -> text 2
+     *
+     * @param prev: old text
+     * @param curr: new text
+     *
+     * @return unidiff patch
+     */
     static std::string diff(std::string prev, std::string curr) {
         diff_match_patch<std::string> dmp;
         return dmp.patch_toText(dmp.patch_make(prev, curr));
     }
 
+    /**
+     * @brief applies patch -> text to get new text
+     *
+     * @param patch: unidiff patch
+     * @param prev: text to patch
+     *
+     * @return patched string
+     */
     static std::string patch(std::string patch, std::string prev) {
         diff_match_patch<std::string> dmp;
         return dmp.patch_apply(dmp.patch_fromText(patch), prev).first;
     }
+
+    /**
+     * @brief checks if there is a difference between strings
+     *
+     * @param prev: previous string
+     * @param curr: current string
+     * @return true if there is a difference
+     */
     static bool isDiff(std::string prev, std::string curr) {
-        return Diff::diff(prev, curr) == "";
+        return Diff::diff(prev, curr) != "";
     }
 };
