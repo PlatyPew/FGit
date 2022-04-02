ce4e8d3af8cb785d7c2e87485ba55e286ed86db4
Platy <daryl.limkz@gmail.com>
2022-04-02 23:07:26 +0800

    feat(reset): added reset functionality
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- include/reset.hpp
+++ include/reset.hpp
@@ -9,12 +9,14 @@ class Reset {
   private:
     std::string path;
     std::string contents = "";
-    bool deleted = false;
 
     void patchContents(std::stack<Blob> blobs);
 
   public:
     Reset(std::string path, std::stack<Blob> blobs);
+
+    std::string getPath();
+    std::string getContents();
 
     static void reset(std::string id);
 };
