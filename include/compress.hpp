c27516c831a77b6a102aaf9ff464a6da0b40c1ed
Platy <daryl.limkz@gmail.com>
2022-04-02 12:49:07 +0800

    docs(commit): added docs for compression functionality
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- include/compress.hpp
+++ include/compress.hpp
@@ -5,12 +5,26 @@
 
 class Compress {
   public:
+    /**
+     * @brief compresses string using gzip
+     *
+     * @param uncompressedData: string to compress
+     *
+     * @return compressed string
+     */
     static std::string compress(std::string uncompressedData) {
         const char* ptr = uncompressedData.data();
         std::size_t size = uncompressedData.size();
         return gzip::compress(ptr, size);
     }
 
+    /**
+     * @brief decompreses string using gzip
+     *
+     * @param compressedData: string to decompress
+     *
+     * @return decompressed string
+     */
     static std::string decompress(std::string compressedData) {
         const char* ptr = compressedData.data();
         std::size_t size = compressedData.size();
