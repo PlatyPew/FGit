d2ca82840a124e5c280ad11e1c57fff47ef15c7d
Platy <daryl.limkz@gmail.com>
2022-04-06 18:22:47 +0800

    docs: added comments for file handler
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- include/filehandler.hpp
+++ include/filehandler.hpp
@@ -3,14 +3,28 @@
 
 class Filehandler {
   public:
+    /**
+     * @brief Checks if file exists
+     *
+     * @param filename: file to check
+     * @return true if file found
+     */
     static bool exists(std::string filename) {
         return std::filesystem::exists(filename);
     }
+
+    /**
+     * @brief read files
+     *
+     * @param filename: file to read
+     * @return contents of the file
+     */
     static std::string read(std::string filename) {
+        if (!exists(filename))
+            throw "File not found";
+
         std::ifstream in(filename);
         std::stringstream data;
-        if (!in)
-            throw "Cannot open file!";
 
         data << in.rdbuf();
         in.close();
@@ -18,10 +32,17 @@ class Filehandler {
         return data.str();
     }
 
+    /**
+     * @brief write contents to the file
+     *
+     * @param filename: file to write
+     * @param contents: contents to write to file
+     */
     static void write(std::string filename, std::string contents) {
         std::ofstream out;
         if (!out)
             throw "Cannot open file!";
+
         out.open(filename);
         out << contents;
         out.close();
