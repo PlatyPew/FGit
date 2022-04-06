ad26359b7ed740f5036ec541c23210b54f413317
Platy <daryl.limkz@gmail.com>
2022-04-06 18:56:56 +0800

    feat: added check if binary in file handler
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- include/filehandler.hpp
+++ include/filehandler.hpp
@@ -1,8 +1,31 @@
 #include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include <string>
 
 class Filehandler {
   public:
+    /**
+     * @brief check if the file is binary
+     *
+     * @param filename: file to check
+     * @return true if binary file
+     */
+    static bool isBinary(std::string filename) {
+        if (!exists(filename))
+            throw "File not found";
+
+        int c;
+        std::ifstream ifs(filename, std::ios::binary);
+        while ((c = ifs.get()) != EOF && c <= 127)
+            ;
+        if (c == EOF)
+            return false;
+
+        return true;
+    }
+
     /**
      * @brief Checks if file exists
      *
@@ -23,7 +46,7 @@ class Filehandler {
         if (!exists(filename))
             throw "File not found";
 
-        std::ifstream in(filename);
+        std::ifstream in(filename, std::ios::binary);
         std::stringstream data;
 
         data << in.rdbuf();
@@ -43,7 +66,7 @@ class Filehandler {
         if (!out)
             throw "Cannot open file!";
 
-        out.open(filename);
+        out.open(filename, std::ios::binary);
         out << contents;
         out.close();
     }
