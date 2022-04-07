025207e4b4116bbe246fb7c3a58ace04e5feb967
Platy <daryl.limkz@gmail.com>
2022-04-07 15:19:59 +0800

    refactor: refactored the main function a bit
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,63 @@
 #include "diff.hpp"
 #include "init.hpp"
 #include "log.hpp"
-
-#include "bin.hpp"
-#include "blob.hpp"
-#include "commit.hpp"
-#include "compress.hpp"
 #include "reset.hpp"
 #include "status.hpp"
-#include <iostream>
-#include <vector>
-
 
 using namespace std;
 
 void runSubCmd(string subCmd, int argc, char** argv) {
-    if (subCmd == "init")
+    // fgit init
+    if (subCmd == "init") {
         Init::init();
-
+        return;
+    }
     // Check if .fgit exists
     if (!Init::exists())
         throw "FGit Repository has not been initialised";
 
-    if (subCmd == "log")
-        Log::log();
-    else if (subCmd == "reset") {
-        if (argc == 3)
-            Reset::reset(argv[2]);
-        else
-            throw "Missing commit id";
-    } else if (subCmd == "diff") {
-        Diff::diff();
+    if (subCmd == "status") {
+        Status::status();
+        return;
     }
 
-    if (subCmd == "status") {
-        Status::status();   
+    // fgit log
+    if (subCmd == "log") {
+        Log::log();
+        return;
+    }
+
+    // fgit diff
+    if (subCmd == "diff") {
+        Diff::diff();
+        return;
+    }
+
+    // fgit reset [commit hash]
+    if (subCmd == "reset") {
+        switch (argc) {
+        case 2:
+            Reset::reset();
+            break;
+
+        case 3:
+            Reset::reset(argv[2]);
+            break;
+
+        default:
+            throw "Too many arguments";
+        }
     }
 }
 
 int main(int argc, char** argv) {
     if (argc >= 2) {
         string subcmd(argv[1]);
-        runSubCmd(subcmd, argc, argv);
+        try {
+            runSubCmd(subcmd, argc, argv);
+        } catch (const string msg) {
+            cerr << msg << endl;
+        }
     }
 
     return 0;
